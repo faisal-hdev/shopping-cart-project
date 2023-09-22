c65abc32247c06e7910f14aa80d8cc84c739df67
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-09-23 00:34:38 +0600

    mobile phone increament and decreament feature addded with total amount cost

--- js/phone.js
+++ js/phone.js
@@ -1,16 +1,33 @@
+function updatePhoneNumber(isIncrease) {
+  const phoneNumberField = document.getElementById("phone-number-field");
+  const phoneNumberString = phoneNumberField.value;
+  const previousPhoneNumber = parseInt(phoneNumberString);
+  let newPhoneNumber;
+  if (isIncrease) {
+    newPhoneNumber = previousPhoneNumber + 1;
+  } else {
+    newPhoneNumber = previousPhoneNumber - 1;
+  }
+  phoneNumberField.value = newPhoneNumber;
+  return newPhoneNumber;
+}
+
+function updatePhoneTotalPrice(newPhoneNumber) {
+  const phoneTotalPrice = newPhoneNumber * 1219;
+  const phoneTotalElement = document.getElementById("phone-total");
+  phoneTotalElement.innerText = phoneTotalPrice;
+}
+
 document
   .getElementById("btn-phone-plus")
   .addEventListener("click", function () {
-    // console.log("plus clicked");
-    const phoneNumberField = document.getElementById("phone-number-field");
-    const phoneNumberString = phoneNumberField.value;
-    const previousPhoneNumber = parseInt(phoneNumberString);
-    const newPhoneNumber = previousPhoneNumber + 1;
-    phoneNumberField.value = newPhoneNumber;
+    const newPhoneNumber = updatePhoneNumber(true);
+    updatePhoneTotalPrice(newPhoneNumber);
   });
 
 document
   .getElementById("btn-phone-minus")
   .addEventListener("click", function () {
-    // console.log("minus cli clicked");
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
   });
